486e0b83e6ce97ea6ca9822c884d61d6098beea2
Mr-Anyone <Random@gmail.com>
2021-08-01 14:44:28 -0400

    Render a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 #include <GLFW/glfw3.h>
 #include "window.h"
 
+#include "camera.h"
+#include "shader.h"
+#include "terrain.h"
+#include "mesh.h"
+
 constexpr int width_g {800}; 
 constexpr int height_g {600}; 
 
@@ -18,14 +23,34 @@ void processInput(GLFWwindow* window)
         glfwSetWindowShouldClose(window, true); 
 }
 
+void draw(const Shader& shader)
+{
+    shader.use(); 
+}
+
+
 int main() 
 {
     GLFWwindow* window = init(width_g, height_g); 
-    
+    Shader shader ("./../res/shader/vertex.glsl", "./../res/shader/fragment.glsl");
+    float vertices [] = {
+        0.5f, -0.5f, 0.0f, 
+        -0.5f, -0.5f, 0.0f, 
+        0.0f, 0.5f, 0.0f
+    };
+    unsigned int indices [] = {
+        0, 1, 2
+    };
+    Mesh mesh (vertices, indices, 9, 3); 
+
     while(!glfwWindowShouldClose(window))
     {
-        processInput(window);
         clearScreen();
+
+        shader.use();
+        mesh.draw(shader);
+
+        processInput(window);
         glfwSwapBuffers(window);
         glfwPollEvents(); 
     }
